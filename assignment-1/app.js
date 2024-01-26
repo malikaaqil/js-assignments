ab16c7c8f0fa3e350822587d2b6d6334457f50e2
Malik Aaqil <malikaaqil2000@gmail.com>
2024-01-26 09:45:02 +0500

    Initial commit

---
+++ assignment-1/app.js
@@ -0,0 +1,55 @@
+// ****** Assignment-1
+
+// Q1
+
+// alert("Hello, Welcome to Our Website!");
+
+
+// Q2
+
+// alert("Error! Please Enter a valid password");
+
+
+// Q3
+
+// alert("Hello \n World");
+
+
+// Q4
+
+// alert("Hello World! 1");
+// alert("Hello World! 2");
+// alert("Hello World! 3");
+
+
+// Q5,6
+
+// Completed
+
+
+// Q7
+
+// var userName;
+
+
+// Q8
+
+// var myName = "Malik Aaqil";
+
+// var myName;
+// myName = "Malik Aaqil";
+
+
+// Q9
+
+// var titleMessage = "Hello World";
+// alert(titleMessage);
+
+
+// Q10
+
+// var studentName = "Malik Aaqil";
+// var studentID = 5678;
+
+// alert("Student Name : " + studentName);
+// alert("Student ID : " + studentID);
