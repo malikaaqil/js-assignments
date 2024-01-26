37eb0578dfd1c9f08508215f48d4dfe7b08aa6c9
Malik Aaqil <malikaaqil2000@gmail.com>
2024-01-26 11:18:36 +0500

    added more questions

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -22,7 +22,7 @@
 // alert("Hello World! 3");
 
 
-// Q5,6
+// Q5, 6
 
 // Completed
 
@@ -34,8 +34,10 @@
 
 // Q8
 
+// // (i)
 // var myName = "Malik Aaqil";
 
+// // (ii)
 // var myName;
 // myName = "Malik Aaqil";
 
@@ -53,3 +55,90 @@
 
 // alert("Student Name : " + studentName);
 // alert("Student ID : " + studentID);
+
+
+// Q11
+
+// var email = "Aaqil@example.com";
+// alert("My Email : " + email);
+
+
+// Q12, 13
+
+// var border = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(border);
+
+
+// Q14
+
+// // (i)
+// var myAge;
+// myAge = 23;
+
+// // (ii)
+// var myAge = 23;
+
+// alert("My Age : " + myAge);
+
+
+// Q15
+
+// skipped
+
+
+// Q16
+
+// var birthYear = 2000;
+
+// alert("My Birth Year was : " + birthYear);
+
+
+// Q17
+
+// var visitorName = "John Doe,";
+// var quantity = 5;
+// var productTitle = "T-Shirts (s)";
+// var storeName = "Outfitter";
+
+// alert(visitorName + " " + "Ordered" + " " + quantity + " " + productTitle + " " + "from" + " " + storeName + "!");
+
+
+// Q18
+
+// // without values
+// var variable1, variable2, variable3;
+
+// // with values
+// var variable1 = "string", variable2 = 1234, variable3 = true;
+
+
+// Q19
+
+// // legal varialble names:
+
+// userName
+// user_name
+// user123
+// user1name
+// userName$
+
+// // illegal variable names:
+
+// user name
+// user-name
+// 123name
+// #name
+// @myName
+
+
+// Q20
+
+// Rules for naming JS variables
+
+// 1. Variable names can only contain letters, numbers, underscores(_) and dollar signs($).For example : $my_1stVariable.
+
+// 2. Variables must begin with a letter, underscore(_) or dollar sign($). For example : $name, _name or name.
+
+// 3. Variable names are case sensitive.
+
+// 4. Variable names should not be JS reserved keywords.
