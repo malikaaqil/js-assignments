40418b3ae9ba8daedd627b6bd10945c057c4d423
Malik Aaqil <malikaaqil2000@gmail.com>
2024-02-02 00:37:31 +0500

    assignment 2

---
+++ assignment-2/app.js
@@ -0,0 +1,24 @@
+// JS Assignment 2
+
+
+// Q1
+// Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser: 
+
+// var initValue = +prompt("Assign a Number to variable ' a ' !");
+
+// document.write("<h1> Result: </h1>" + "<br />");
+// document.write("The value of a is: " + initValue  + "<br />");
+
+// document.write("<b>.........................................</b>" + "<br /> <br />");
+
+// document.write("The value of ++a is: " + ++initValue + "<br />");
+// document.write("Now the Value of a is: " + initValue + "<br /> <br />");
+
+// document.write("The Value of a++ is: " + initValue++ + "<br />");
+// document.write("Now the Value of a is: " + initValue + "<br /> <br />");
+
+// document.write("The Value of --a is: " + --initValue + "<br />");
+// document.write("Now the Value of a is: " + initValue + "<br /> <br />");
+
+// document.write("The Value of a-- is: " + initValue-- + "<br />");
+// document.write("Now the Value of a is: " + initValue);
