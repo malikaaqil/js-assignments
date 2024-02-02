505afcda095474b1fb7ab4c9992ace7561dbaae7
Malik Aaqil <malikaaqil2000@gmail.com>
2024-02-02 11:52:51 +0500

    completed all questions

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -64,4 +64,28 @@
 // b) Total marks for each subject is 100, store it in another variable.
 // c) Take obtained marks for first subject from user and stored it in different variable.
 // d) Take obtained marks for remaining 2 subjects from user and store them in variables.
-// e) Now calculate total marks and percentage and show the result in browser like this. (Hint: user table) 
+// e) Now calculate total marks and percentage and show the result in browser. (Hint: user table) 
+
+
+// var subject1 = prompt("Enter your first subject Name:").toUpperCase();
+// var subject2 = prompt ("Enter your second subject Name: ").toUpperCase();
+// var subject3 = prompt ("Enter your third subjcet Name: ").toUpperCase();
+
+// var subj1Marks = +prompt("Enter your " + subject1 + " obtained marks: ")
+// var subj2Marks = +prompt("Enter your " + subject2 + " obtained marks: ")
+// var subj3Marks = +prompt("Enter your " + subject3 + " obtained marks: ")
+
+// var totalMarks = 100;
+// var totalObtainedMarks = subj1Marks + subj2Marks + subj3Marks;
+
+// var result = (totalObtainedMarks / (totalMarks*3) * 100);
+
+// document.write("<h1>Result:</h1>")
+
+// document.write(`<table border='2'>
+//  <tr> <th> Subject </th> <th> Total Marks </th> <th> Obtained marks </th> <th> Percentage </th> </tr>
+//  <tr> <td>${subject1}</td> <td>${totalMarks}</td> <td>${subj1Marks}</td> <td>${subj1Marks/totalMarks*100}%</td> </tr>
+//  <tr> <td>${subject2}</td> <td>${totalMarks}</td> <td>${subj2Marks}</td> <td>${subj2Marks/totalMarks*100}%</td> </tr>
+//  <tr> <td>${subject3}</td> <td>${totalMarks}</td> <td>${subj3Marks}</td> <td>${subj3Marks/totalMarks*100}%</td> </tr>
+//  <tr> <th>Total</th> <th>${totalMarks*3}</th> <th>${totalObtainedMarks}</th> <th>${result}%</th> </tr>
+//  </table>`)
