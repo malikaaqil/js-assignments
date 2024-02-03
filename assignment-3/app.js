e94ed879a0e2cc3d3220e6a062ae13d42cc924db
Malik Aaqil <malikaaqil2000@gmail.com>
2024-02-03 09:20:56 +0500

    updated

---
+++ assignment-3/app.js
@@ -0,0 +1,30 @@
+// JS Assignment 3
+
+// Q1
+// Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this: “Welcome to city of lights”
+
+// var userCity = prompt("Enter a city name: ").toLowerCase();
+
+// if(userCity === "karachi"){
+//     alert("Welcome to city of lights!")
+// }else{
+//     alert("Not the city of lights!")
+// }
+
+
+// Q2
+// Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// var userGender = prompt("Enter Your Gender: ", "Male").toLowerCase();
+
+// if(userGender === "male"){
+
+//     alert("Good Morning Sir")
+
+// }else if (userGender === "female"){
+
+//     alert("Good Morning Ma’am")
+
+// }else{
+//     alert("Please Enter Correct Gender!!")
+// }
